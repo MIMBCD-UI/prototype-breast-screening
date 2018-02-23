7d698391f67dc6c67dbeeaba7b260ae6f78dbfbc
FMCalisto <FMCalisto@users.noreply.github.com>
2018-02-23 21:06:07 +0000

    [CREATE] Timestamp Closes #1

--- src/js/setupButtons.js
+++ src/js/setupButtons.js
@@ -1,141 +1,188 @@
-
 function setupButtons(studyViewer) {
-    // Get the button elements
-    var buttons = $(studyViewer).find('button');
+  // Get the button elements
+  var buttons = $(studyViewer).find('button');
 
-    // Tool button event handlers that set the new active tool
+  // Tool button event handlers that set the new active tool
 
-    // WW/WL
-    $(buttons[0]).on('click touchstart', function() {
-        disableAllTools();
-        forEachViewport(function(element) {
-            cornerstoneTools.wwwc.activate(element, 1);
-            cornerstoneTools.wwwcTouchDrag.activate(element);
-        });
+  // WW/WL
+  $(buttons[0]).on('click touchstart', function() {
+    disableAllTools();
+    forEachViewport(function(element) {
+      cornerstoneTools.wwwc.activate(element, 1);
+      cornerstoneTools.wwwcTouchDrag.activate(element);
+    });
+  });
+
+  // Invert
+  $(buttons[1]).on('click touchstart', function() {
+    disableAllTools();
+    forEachViewport(function(element) {
+      var viewport = cornerstone.getViewport(element);
+      // Toggle invert
+      if (viewport.invert === true) {
+        viewport.invert = false;
+      } else {
+        viewport.invert = true;
+      }
+      cornerstone.setViewport(element, viewport);
+    });
+  });
+
+  // Zoom
+  $(buttons[2]).on('click touchstart', function() {
+    disableAllTools();
+    forEachViewport(function(element) {
+      // 5 is right mouse button and left mouse button
+      cornerstoneTools.zoom.activate(element, 5);
+      cornerstoneTools.zoomTouchDrag.activate(element);
+    });
+  });
+
+  // Pan
+  $(buttons[3]).on('click touchstart', function() {
+    disableAllTools();
+    forEachViewport(function(element) {
+      // 3 is middle mouse button and left mouse button
+      cornerstoneTools.pan.activate(element, 3);
+      cornerstoneTools.panTouchDrag.activate(element);
+    });
+  });
+
+  // Stack scroll
+  $(buttons[4]).on('click touchstart', function() {
+    disableAllTools();
+    forEachViewport(function(element) {
+      cornerstoneTools.stackScroll.activate(element, 1);
+      cornerstoneTools.stackScrollTouchDrag.activate(element);
+    });
+  });
+
+  // Length measurement
+  $(buttons[5]).on('click touchstart', function() {
+    disableAllTools();
+    forEachViewport(function(element) {
+      cornerstoneTools.length.activate(element, 1);
+    });
+  });
+
+  // Freehand ROI draw
+  $(buttons[6]).on('click touchstart', function() {
+    forEachViewport(function(element) {
+      cornerstoneTools.freehand.activate(element, 1);
+    });
+  });
+
+  // JSON Save Button
+  $(buttons[7]).on('click touchstart', function() {
+    disableAllTools();
+    forEachViewport(function(element) {
+
+      /**
+       * time_stamp => YYYYMMDDHHmmSS
+       *
+       * Y: Year
+       * M: Month
+       * D: Day
+       * H: Hour
+       * m: minute
+       * S: Second
+       *
+       */
+
+      var currentDate = new Date();
+      var year = currentDate.getFullYear();
+      var yearString = year.toString();
+      //Be careful! January is 0 not 1
+      var month = currentDate.getMonth() + 1;
+      var monthString = (month < 9 ? '0': '') + (month);
+      var date = currentDate.getDate();
+      var dateString = (date < 9 ? '0': '') + (date);
+      var hours = currentDate.getHours();
+      var hoursString = (hours < 9 ? '0': '') + (hours);
+      var minutes = currentDate.getMinutes();
+      var minutesString = (minutes < 9 ? '0': '') + (minutes);
+      var seconds = currentDate.getSeconds();
+      var secondsString = (seconds < 9 ? '0': '') + (seconds);
+      var totalDate = yearString + monthString + dateString;
+      var totalTime = hoursString + minutesString + secondsString;
+      var timeStamp = totalDate + totalTime;
+
+      var toolData = cornerstoneTools.getToolState(element, 'freehand');
+      var studyRoiData = studyViewer.roiData;
+      var studyCurrentStack = studyRoiData.currentStack;
+      var stack = studyRoiData.stacks[studyCurrentStack];
+      var dataStudyId = studyRoiData.studyId;
+      var fileNameToSave = timeStamp + dataStudyId + '.json';
+
+      if (toolData === undefined) {
+        return;
+      }
+
+      console.log("TimeStamp: ", timeStamp);
+
+      // The name of the file is the system studyId of the patient:
+      // timeStamp + studyId
+      // Example:
+      // 20180223203337 + 123456789
+      // Output File:
+      // 20180223203337123456789.json
+
+      console.log("Saved File Name: ", fileNameToSave);
+      stack.freehand = toolData.data;
+
+      console.log(studyRoiData);
+
+      // save json
+      download(
+        studyRoiData,
+        fileNameToSave,
+        'application/octet-stream'
+      );
+
+    });
+  });
+
+  // Tooltips
+  $(buttons[0]).tooltip();
+  $(buttons[1]).tooltip();
+  $(buttons[2]).tooltip();
+  $(buttons[3]).tooltip();
+  $(buttons[4]).tooltip();
+  $(buttons[5]).tooltip();
+  $(buttons[7]).tooltip();
+  $(buttons[8]).tooltip();
+
+  function download(data, name, type) {
+    var link = document.createElement("a");
+    var data = JSON.stringify(data, null, 4);
+    var blob = new Blob([data], {
+      type: 'application/octet-stream'
+    });
+    var url = URL.createObjectURL(blob, {
+      type: type
     });
 
-    // Invert
-    $(buttons[1]).on('click touchstart', function() {
-        disableAllTools();
-        forEachViewport(function(element) {
-            var viewport = cornerstone.getViewport(element);
-            // Toggle invert
-            if (viewport.invert === true) {
-                viewport.invert = false;
-            } else {
-                viewport.invert = true;
-            }
-            cornerstone.setViewport(element, viewport);
-        });
-    });
+    link.setAttribute('href', url);
+    link.setAttribute('download', name);
 
-    // Zoom
-    $(buttons[2]).on('click touchstart', function() {
-        disableAllTools();
-        forEachViewport(function(element) {
-            // 5 is right mouse button and left mouse button
-            cornerstoneTools.zoom.activate(element, 5);
-            cornerstoneTools.zoomTouchDrag.activate(element);
-        });
-    });
-
-    // Pan
-    $(buttons[3]).on('click touchstart', function() {
-        disableAllTools();
-        forEachViewport(function(element) {
-            // 3 is middle mouse button and left mouse button
-            cornerstoneTools.pan.activate(element, 3);
-            cornerstoneTools.panTouchDrag.activate(element);
-        });
-    });
-
-    // Stack scroll
-    $(buttons[4]).on('click touchstart', function() {
-        disableAllTools();
-        forEachViewport(function(element) {
-            cornerstoneTools.stackScroll.activate(element, 1);
-            cornerstoneTools.stackScrollTouchDrag.activate(element);
-        });
-    });
-
-    // Length measurement
-    $(buttons[5]).on('click touchstart', function() {
-        disableAllTools();
-        forEachViewport(function(element) {
-            cornerstoneTools.length.activate(element, 1);
-        });
-    });
-
-    // Freehand ROI draw
-    $(buttons[6]).on('click touchstart', function() {
-        forEachViewport(function(element) {
-            cornerstoneTools.freehand.activate(element, 1);
-        });
-    });
-
-    // JSON Save Button
-    $(buttons[7]).on('click touchstart', function() {
-        disableAllTools();
-        forEachViewport(function(element) {
-            var toolData = cornerstoneTools.getToolState(element, 'freehand')
-            if (toolData === undefined) {
-                return;
-            }
-
-            var stack = studyViewer.roiData.stacks[studyViewer.roiData.currentStack];
-            stack.freehand = toolData.data;
-
-            console.log(studyViewer.roiData)
-
-            // save json
-            download(studyViewer.roiData, 'test.json', 'application/octet-stream');
-        });
-    });
-
-    // Tooltips
-    $(buttons[0]).tooltip();
-    $(buttons[1]).tooltip();
-    $(buttons[2]).tooltip();
-    $(buttons[3]).tooltip();
-    $(buttons[4]).tooltip();
-    $(buttons[5]).tooltip();
-    $(buttons[7]).tooltip();
-    $(buttons[8]).tooltip();
-
-    function download(data, name, type) {
-        var link = document.createElement("a");
-        var data = JSON.stringify(data, null, 4);
-        var blob = new Blob([data], {
-            type: 'application/octet-stream'
-        });
-        var url = URL.createObjectURL(blob, {type: type});
-
-        link.setAttribute('href', url);
-        link.setAttribute('download', name);
-
-        var event = document.createEvent('MouseEvents');
-        event.initMouseEvent(
-            'click',
-            true,
-            true,
-            window,
-            1,
-            0,
-            0,
-            0,
-            0,
-            false,
-            false,
-            false,
-            false,
-            0,
-            null
-        );
-        link.dispatchEvent(event);
-    }
+    var event = document.createEvent('MouseEvents');
+    event.initMouseEvent(
+      'click',
+      true,
+      true,
+      window,
+      1,
+      0,
+      0,
+      0,
+      0,
+      false,
+      false,
+      false,
+      false,
+      0,
+      null
+    );
+    link.dispatchEvent(event);
+  }
 };
-
-
-
-
-
