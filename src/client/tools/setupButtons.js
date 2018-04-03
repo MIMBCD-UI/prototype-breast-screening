bfc0d94fa863e0495f145594af6ecbb9c75511d5
FMCalisto <FMCalisto@users.noreply.github.com>
2018-04-03 17:47:08 +0100

    [BACKUP] Backed Up

--- src/client/tools/setupButtons.js
+++ src/client/tools/setupButtons.js
@@ -19,7 +19,6 @@ var secondsString = (seconds < 9 ? '0' : '') + (seconds);
 var totalDate = yearString + monthString + dateString;
 var totalTime = hoursString + minutesString + secondsString;
 var timeStamp = totalDate + totalTime;
-var currentElement;
 
 // var http = require('http');
 
@@ -91,7 +90,6 @@ const setupButtons = (studyViewer) => {
   // Freehand ROI draw
   $(buttons[5]).on('click touchstart', function() {
     forEachViewport(function(element) {
-      currentElement = element;
       cornerstoneTools.freehand.activate(element, 1);
     });
   });
@@ -213,20 +211,3 @@ const setupButtons = (studyViewer) => {
     link.dispatchEvent(event);
   }
 };
-
-$(document).keyup(function(e) {
-  if (e.keyCode == 27 && currentElement !== undefined) { // escape key maps to keycode `27`
-  var toolStateManager = cornerstoneTools.getElementToolStateManager(currentElement);
-  var freehandToolState = toolStateManager.get(currentElement, 'freehand');
-  if(freehandToolState){
-    freehandToolState.data[freehandToolState.data.length-1].handles.pop();
-    // for(var i=0; i<freehandToolState.data.length; i++){
-
-    // }
-    cornerstoneTools.freehand.getConfiguration().currentHandle--;
-
-    cornerstone.updateImage(currentElement);
-  }
-
- }
-});
