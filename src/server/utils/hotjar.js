03005186d04d243014fd9b23f1cba4007ea9acbe
FMCalisto <FMCalisto@users.noreply.github.com>
2018-04-09 18:39:47 +0100

    [UPDATE] Hotjar

--- src/server/utils/hotjar.js
+++ src/server/utils/hotjar.js
@@ -14,3 +14,5 @@
   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
   a.appendChild(r);
 })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+console.log("Hotjar is running...");
