30814ca33f5b29c0cc0bd7ea59eaeefad617cc94
FMCalisto <FMCalisto@users.noreply.github.com>
2018-04-17 15:37:24 +0100

    [UPDATE] Hotjar Scripts

--- src/server/utils/hotjar.js
+++ src/server/utils/hotjar.js
@@ -1,3 +1,82 @@
+/* ================================================== */
+/** Base Configuration Variables */
+/* ================================================== */
+
+var configFileDir = '../../../config/';
+var fileExtension = '.json';
+var requestValue = 'GET';
+
+/* ================================================== */
+
+
+/* ================================================== */
+/** Environment Configuration Variables */
+/* ================================================== */
+
+var configEnvFileName = 'env';
+var envFileFull = configEnvFileName + fileExtension;
+var configEnvFilePath = configFileDir + envFileFull;
+
+/* ================================================== */
+
+
+/* ================================================== */
+/**
+ *
+ * Load JSON configuration data from the sercer using
+ * GET HTTP request
+ *
+ */
+/* ================================================== */
+
+var requestEnv = new XMLHttpRequest();
+
+requestEnv.open(requestValue, configEnvFilePath, false);
+requestEnv.send(null);
+
+var configEnvObject = JSON.parse(requestEnv.responseText);
+var configFileName = configEnvObject.environment;
+
+/* ================================================== */
+/* ================================================== */
+/* ================================================== */
+
+
+/* ================================================== */
+/** Main Configuration Variables */
+/* ================================================== */
+
+var fileFull = configFileName + fileExtension;
+var configFilePath = configFileDir + fileFull;
+
+/* ================================================== */
+
+
+/* ================================================== */
+/**
+ *
+ * Load JSON configuration data from the sercer using
+ * GET HTTP request
+ *
+ */
+/* ================================================== */
+
+var request = new XMLHttpRequest();
+
+request.open(requestValue, configFilePath, false);
+request.send(null);
+
+var configObject = JSON.parse(request.responseText);
+var mainServerValue = configObject.mainServer;
+var hotjarValue = mainServerValue[0].hotjar;
+var hotjarNumber = Number(hotjarValue);
+
+console.log("Hotjar Number:\n", hotjarNumber);
+
+/* ================================================== */
+/* ================================================== */
+/* ================================================== */
+
 /**
  *
  * Hotjar Tracking Code
@@ -6,11 +85,9 @@
  *
  */
 
-
-
 (function(h,o,t,j,a,r){
   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-  h._hjSettings={hjid:841221,hjsv:6};
+  h._hjSettings={hjid:hotjarNumber,hjsv:6};
   a=o.getElementsByTagName('head')[0];
   r=o.createElement('script');r.async=1;
   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
