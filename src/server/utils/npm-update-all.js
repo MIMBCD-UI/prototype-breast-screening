86d5176a6eee28fc574bed6a56ae82464abdcb7f
FMCalisto <FMCalisto@users.noreply.github.com>
2018-07-17 11:08:06 +0100

    [FIX] NPM Update All

--- src/server/utils/npm-update-all.js
+++ src/server/utils/npm-update-all.js
@@ -10,8 +10,8 @@
 
 // In your server.js/index.js , run following code:
 
-var updateAll = require('npm-update-all');
-var json = require('./../../../package.json');
+// var updateAll = require('npm-update-all');
+// var json = require('./../../../package.json');
 
 /**
  * @function
